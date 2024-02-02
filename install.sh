d2b67e298b545017385b4c63ae81d66e40bfe667
Konstantin Dyachenko <drakonard@gmail.com>
2024-02-02 14:21:18 +0000

    Simplify installation process

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker run -d -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.13.1
-pyenv install 3.11 -f
-pyenv global 3.11
+# pyenv install 3.11 -f
+# pyenv global 3.11
 pip install click 'elasticsearch<7.14.0'
 pip install -U sentence-transformers
